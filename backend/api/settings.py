0a7a8c61f2f31f0f88f07533637f5f2b3d60c549
unknown <ahmedgamal1452@gmail.com>
2024-02-13 19:39:15 +0200

    change sql

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres_db',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'db',  # This will be the service name in docker-compose.yml
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#      'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'db',  # This will be the service name in docker-compose.yml
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
